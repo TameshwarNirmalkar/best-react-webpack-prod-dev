d58e65abe13658ecf40ff8f70b445daa24629476
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2020-02-06 10:48:19 +0530

    added callback to useState

--- src/components/vehicle-change/vehicle-change-component.js
+++ src/components/vehicle-change/vehicle-change-component.js
@@ -1,14 +1,61 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Select from 'react-select';
 import styles from './vehicle-change.scss';
 
+const options = [
+  { value: '1', label: 'Emp1' },
+  { value: '2', label: 'Emp2' },
+  { value: '3', label: 'Emp3' },
+];
+
 const VehicleChangeComponent = () => {
+  const [selectedOption, setSelectedOption] = useState({ value: '2', label: 'Emp2' });
+  const [masterData, setMasterData] = useState({
+    masterField: {
+      fieldOne: "a",
+      fieldTwo: {
+        fieldTwoOne: "b",
+        fieldTwoTwo: "c",
+      }
+    }
+  });
+
+  const handleChange = async (val) => {
+    const selOp = { value: val.value, label: val.label };
+    // setSelectedOption(selOp);
+    const valAwait = await setSelectedOption(prevState => ({ ...prevState, value: val.value, label: val.label }));
+    console.log('selectedOption:::', selectedOption, valAwait);
+
+    fetch(`https://jsonplaceholder.typicode.com/todos/${selectedOption.value}`)
+      .then(response => response.json())
+      .then(res => {
+        console.log('REsponse: ', res);
+        const data = {
+          masterField: { ...res }
+        };
+        setMasterData({ ...masterData, ...data });
+      });
+
+
+  };
+
   return (
     <Fragment>
       <div className={styles['vehicle-comp']}>
         <div className="container">
           <div className="row">
-            <div className="col"> Select Model </div>
-            <div className="col"> Select Variant </div>
+            <div className="col">
+              <span>Select Model</span>
+              <Select
+                value={selectedOption}
+                onChange={handleChange}
+                options={options} />
+
+            </div>
+            <div className="col">
+              Select Variant
+              {JSON.stringify(masterData)}
+            </div>
             <div className="w-100" />
             <div className="col"> col </div>
             <div className="col"> col </div>
